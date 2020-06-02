a8607fface3e614c404f2f1a360b2840b3742ddd
Abdulmateen Tairu <tay2druh@gmail.com>
2020-06-02 21:04:33 +0100

    done tasks endpoints

---
+++ controllers/task.js
@@ -0,0 +1,86 @@
+const { task, user, Sequelize : { Op } } = require('../models/index');
+
+const baseController = require('./base')
+
+class taskController extends baseController{
+  constructor() {
+    super();
+
+  }
+
+  async getAllTasks(req, res){
+
+    let {score, assignees, status, name, assignees_field} = req.query;
+    let query = {}
+
+    if (score){
+      query.score = {
+        [Op.gte]: Number(score) || 0
+      }
+    }
+
+    if (name){
+      query.name = {
+        [Op.substring]: name
+      }
+    }
+
+    if (status){
+      query.status = {
+        [Op.or]: status.split(",")
+      }
+    }
+
+    if (assignees){
+      assignees = assignees.split(",")
+
+      let users, listOfUsersId = [];
+
+      if (assignees_field === "surname"){
+        users = await user.findAll({where:{surname : {[Op.or]: assignees}}})
+      }
+      else if (assignees_field === "name"){
+        users = await user.findAll({where: {name: {[Op.or]: assignees}}})
+      }
+      else if (assignees_field === "id"){
+        users = await user.findAll({where: {id: {[Op.or]: assignees}}})
+      }
+
+      if (users.length){
+        users.forEach(element=>{
+          listOfUsersId.push(element.id)
+        })
+        assignees = listOfUsersId
+        query.userId = {
+          [Op.or]: assignees
+        }
+      }
+      else {
+        query.userId = {
+          [Op.or]: [null]
+        }
+      }
+
+    }
+
+    let response = await task.findAll({
+      where: query
+    })
+
+    if (!response.length){
+      return super.sendSuccess(res, [], "No task was gotten")
+    }
+
+    return super.sendSuccess(res, response, "Tasks Gotten")
+  }
+
+  async createTask(req, res){
+    let { name = "", description = "", score = 0, status = "", userId, projectId} = req.body;
+    let toBeSentData = {name, status, description, userId, score, projectId}
+    let instance = await task.create(toBeSentData)
+    return super.sendSuccess(res, instance, "Task Created", 201)
+  }
+
+}
+
+module.exports = new taskController();
--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,19 @@
 const { Router } = require('express');
+
 const userController = require('../controllers/user');
-const projectController = require('../controllers/project')
+const projectController = require('../controllers/project');
+const taskController = require('../controllers/task');
 
 
 const router = Router();
 
-router.get('/user/getall', userController.getUsers);
-router.post('/user/create', userController.createUser);
+router.get('/user', userController.getUsers);
+router.post('/user', userController.createUser);
 
-router.post('/project/create', projectController.createProject);
-router.get('/project/getall', projectController.getAllProjects)
+router.post('/project', projectController.createProject);
+router.get('/project', projectController.getAllProjects)
+
+router.post('/task', taskController.createTask);
+router.get('/task', taskController.getAllTasks)
 
 module.exports = router
